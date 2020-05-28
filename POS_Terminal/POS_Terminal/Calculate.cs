540c6dbe428cea0cd0699e3ce75405c241d3670c
andy <asmithdev91@gmail.com>
2020-05-27 20:24:56 -0400

    12 menu items made. Added Calculate class for calculation methods.

---
+++ POS_Terminal/POS_Terminal/Calculate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS_Terminal
+{
+    public abstract class Calculate
+    {
+        // Where the calculation methods will be defined.
+    }
+}
--- POS_Terminal/POS_Terminal/Product.cs
+++ POS_Terminal/POS_Terminal/Product.cs
@@ -6,6 +6,12 @@ namespace POS_Terminal
 {
     public class Product
     {
+        // Needs a constructor that pulls the info from menu.txt
+        // to define the values of the below variables.
+
+        // Possibly need a Drink subclass so the user can say
+        // what size drink they want when checking out.
+
         // Name
         // Category : Food, Drink
         // Description
